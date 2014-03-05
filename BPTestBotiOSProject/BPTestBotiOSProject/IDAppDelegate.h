77cff5d4cce4907ca862a3251cb8497600d413ca
tkc <tkc@idealdev.dk>
2014-03-05 21:30:21 +0100

    Created repo

---
+++ BPTestBotiOSProject/BPTestBotiOSProject/IDAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  IDAppDelegate.h
+//  BPTestBotiOSProject
+//
+//  Created by Thomas Kalhøj Clemensen on 05/03/14.
+//  Copyright (c) 2014 iDeal Development. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface IDAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ BPTestBotiOSProject/BPTestBotiOSProject/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  BPTestBotiOSProject
+//
+//  Created by Thomas Kalhøj Clemensen on 05/03/14.
+//  Copyright (c) 2014 iDeal Development. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "IDAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([IDAppDelegate class]));
+    }
+}
